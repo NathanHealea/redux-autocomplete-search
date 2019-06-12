750e0deecb0e5cad04b752dd92beb4b3c80a6f18
nathanhealea <nate.healea@gmail.com>
2019-06-12 00:25:01 -0700

    WIP fetching all pages for getCards

--- src/actions/Cards.js
+++ src/actions/Cards.js
@@ -8,20 +8,39 @@ export const FETCH_CARDS_FAILURE = "FETCH_CARDS_FAILURE";
 function getCards(cardname) {
   console.log("getCards");
   return axios(`https://api.scryfall.com/cards/search?unique=prints&q=${encodeURI(deburr(cardname.trim()).toLowerCase().replace(/\s+/g, '-'))}`)
-  ;
+    ;
 }
 
-export function fetchCards(cardname) {
+function fetchAdditionalCards(url) {
+  return dispatch => {
+    dispatch(fetchAdditionalCardsBegin())
+
+    return axios(url)
+      .then(response => {
+
+      })
+  }
+}
+
+export function fetchCards(cardname, page = 1) {
   return dispatch => {
     dispatch(fetchCardsBegin());
 
     return getCards(cardname)
       .then(response => {
-        if (response.status === 200) {
-          dispatch(fetchCardsSuccess(response.data.data));
+
+        if (response.state === 200 && response.data.has_more) {
+          dispatch(fetchAdditionalCards(response.data.next_page))
         } else {
-          dispatch(fetchCardsFailure("An unknown error occured"));
+          if (response.status === 200) {
+            dispatch(fetchCardsSuccess(response.data.data));
+
+          } else {
+            dispatch(fetchCardsFailure("An unknown error occured"));
+          }
         }
+
+
       })
       .catch(error => {
         dispatch(fetchCardsFailure(error));
